c0b94c008fc3e3311e8397ee67bbcad8d1128b1f
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-07-17 23:47:28 +0330

    feat: Show history table

--- src/components/Header/details/IndexTable.jsx
+++ src/components/Header/details/IndexTable.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "./IndexTable.module.scss";
 
-const IndexTable = () => {
+const IndexTable = ({ data }) => {
+  // Virtual scroll for loading data
+  const [visibleData, setVisibleData] = useState([]);
+  const [lastRow, setLastRow] = useState(40);
+  const tableContainerRef = useRef(null);
+  const rowHeight = 40;
+  const tableHeight = 400;
+
+  const handleScroll = () => {
+    const scrollTop = tableContainerRef.current.scrollTop;
+
+    if (!visibleData.length) {
+      const newData = data.slice(0, lastRow);
+      setVisibleData(newData);
+    } else {
+      if (scrollTop > tableHeight * (lastRow / 20)) {
+        const newData = data.slice(lastRow, lastRow + rowHeight);
+        setVisibleData((prevState) => [...prevState, ...newData]);
+        setLastRow(lastRow + rowHeight);
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleScroll();
+  }, [data]);
+
   return (
-    <div className={styles["indexTable-wrapper"]}>
+    <div
+      className={styles["indexTable-wrapper"]}
+      onScroll={handleScroll}
+      ref={tableContainerRef}
+    >
       <table className={styles.table}>
         <thead>
           <tr>
@@ -13,12 +44,26 @@ const IndexTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {!visibleData.length ? (
+            <tr>
+              <td colSpan={4} className={styles.loading}>
+                درحال بارگزاری...
+              </td>
+            </tr>
+          ) : (
+            visibleData.map((item, index) => {
+              const date = new Date(item[0]).toLocaleString();
+
+              return (
+                <tr key={index}>
+                  <td>{date}</td>
+                  <td>{item[3]}</td>
+                  <td>{item[1]}</td>
+                  <td>{item[2]}</td>
+                </tr>
+              );
+            })
+          )}
         </tbody>
       </table>
     </div>
