a029a17d5edc21b8ec2abaaa793efd23f0509572
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-07-17 16:05:30 +0330

    feat: Add fetch History data function

--- src/utils/api/tsetmc-api.js
+++ src/utils/api/tsetmc-api.js
@@ -17,3 +17,28 @@ export const getAllIndex = async () => {
 
   return await response.json();
 };
+
+/**
+ * Fetches the details of a given index
+ * @param {string} id - ID of the index, e.g. '32097828799138957'
+ * @returns {Promise<Object[]>} Response array of all history
+ */
+
+export const getIndexHistory = async (id, signal = {}) => {
+  const url = `${baseUrl}/GetIndexB2History/${id}`;
+  try {
+    const response = await fetch(url, {
+      signal,
+    });
+
+    if (!response.ok) {
+      const message = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+
+    return await response.json();
+  } catch (error) {
+    // TODO: Handle error
+    console.log(error);
+  }
+};
